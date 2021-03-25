1b1bed257345815caf1fe214a6ed7f96606cacfa
Rmolimock <495@holbertonschool.com>
2021-03-25 16:34:12 +0000

    basic flask app and app engine requirements

---
+++ main.py
@@ -0,0 +1,68 @@
+"""
+ENTRY POINT
+"""
+from flask import Flask, jsonify, render_template, url_for
+from flask_cors import (CORS, cross_origin)
+from uuid import uuid4
+from os import environ
+
+
+
+app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+if "FLASK_SECRET_KEY" in environ:
+    app.secret_key = environ["FLASK_SECRET_KEY"]
+else:
+    environ["FLASK_SECRET_KEY"] = str(uuid4())
+CORS(app, resources={r"*": {"origins": "*"}})
+
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index():
+    return 'hello world'
+
+
+"""
+ERROR HANDLERS
+"""
+
+@app.errorhandler(400)
+def bad_request(error) -> str:
+    """
+    Request is bad
+    """
+    return jsonify({"error": "Bad Request, https required"}), 400
+
+
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """
+    Not found handler
+    """
+    return jsonify({"error": "Not found"}), 404
+
+
+@app.errorhandler(403)
+def Forbidden(error) -> str:
+    """
+    Forbidden handler
+    """
+    return jsonify({"error": "Forbidden"}), 403
+
+
+@app.errorhandler(401)
+def Unauthorized(error) -> str:
+    """
+    Unauthorized handler
+    """
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+
+if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
+# [END gae_python38_app]
