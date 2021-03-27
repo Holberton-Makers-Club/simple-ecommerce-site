8b4e220876cbafa6152e27f1888e65b53aa36f3c
Rmolimock <495@holbertonschool.com>
2021-03-27 17:14:39 +0000

    store route requests products api and fills in with jinja

---
+++ helpers.py
@@ -0,0 +1,5 @@
+def build_url(endpoint):
+    # in deployment
+    # in development
+    # return 'simple-ecommerce-site-308706.uc.r.appspot.com' + endpoint
+    return 'http://127.0.0.1:8080/' + endpoint
--- routes/store.py
+++ routes/store.py
@@ -1,8 +1,15 @@
 from flask import render_template, Blueprint
+import requests
+from helpers import build_url
 
 
 storefront = Blueprint("store", __name__, url_prefix="/store")
 
 @storefront.route('/', methods=['GET'], strict_slashes=False)
 def index():
-    return render_template('store.html')
+    data = {}
+    r = requests.get(build_url('api/products')).json()
+    if not r.get('status') == 'OK':
+        data['msg'] = 'API error'
+    data['products'] = r.get('products')
+    return render_template('store.html', data=data)
